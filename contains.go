7f021f5580e0ec842d079833217f6c667452b901
Atul Tiwari <atltvri@gmail.com>
2020-11-30 00:07:14 +0530

    another commit

--- contains.go
+++ contains.go
@@ -8,4 +8,11 @@ func main(){
 	var command="walk outside"
 	var exit=strings.Contains(command, "walk outside")
 	fmt.Println("You leave the cave:",exit)
+
+	//qc 3.1
+	//1
+	var wearShades=true
+	//2
+	
+	wearShades=strings.Contains(command,"read")
 }
